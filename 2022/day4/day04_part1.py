ef661870ad7843e67530f115b0c6ce1b8ab417e7
Luis <semproms@outlook.com>
2022-12-08 09:32:57 +0100

    unnecessary print()

--- 2022/day4/day04_part1.py
+++ 2022/day4/day04_part1.py
@@ -7,10 +7,6 @@ def processTask(line):
     range1 = range(int(firstElveRange.split('-')[0]), int(firstElveRange.split('-')[1]) + 1)
     range2 = range(int(secondElveRange.split('-')[0]), int(secondElveRange.split('-')[1]) + 1)
     
-    if range1.start in range2 and range1[-1] in range2 or range2.start in range1 and range2[-1] in range1:
-        for i in range1:
-            print(i)
-    
     return range1.start in range2 and range1[-1] in range2 or range2.start in range1 and range2[-1] in range1
 
 for line in file:
